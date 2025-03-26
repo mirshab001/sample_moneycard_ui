a652b0485d1fe03103f63c73e2816271a80dfbda
mirshab001 <mirshabpm01@gmail.com>
2025-03-26 15:07:36 +0530

    settings edited

---
+++ lib/setting_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Text("hii..."),
+        ),
+      ),
+    );
+  }
+}
