92a10b1028470e64e6687fd2ecd787cb11175777
mirshab001 <mirshabpm01@gmail.com>
2025-03-26 15:09:19 +0530

    Create profile.dart

---
+++ lib/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Text("hii...hello world"),
+        ),
+      ),
+    );
+  }
+}
